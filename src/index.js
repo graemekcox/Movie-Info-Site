29aa9394e4b2b00e8e011c623e9b5c5c9d16df97
graemekcox <graeme.k.cox@gmail.com>
2020-04-11 15:39:37 -0400

    Update search functionality

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const Movie = ({movie}) => {
     return (
     <div>
         <Container className="movie">
-            <div class="movie-body">
+            <div className="movie-body">
                 <h2 className="movie-title">{JSON.stringify(movie.title)}</h2>
                 <img src={`${img_base}${movie.poster_path}`} alt="Poster" width="200"/>
                 <h5 className="movie-year">{JSON.stringify(movie.release_date)}</h5>
@@ -23,13 +23,21 @@ const Movie = ({movie}) => {
 )
 }
 
-class App extends React.Component {
-    state = {
-        movies: [],
-        title: ''
+class MovieForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {search_title: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    componentDidMount(){
+    handleChange(event) {
+        this.setState({search_title: event.target.value});
+    }
+
+    handleSubmit(event) {
+        // alert('A movie was submitted: '  + this.state.value);
         fetch('https://api.themoviedb.org/3/movie/76341?api_key=007d7263d6c65904a9285e1a3754a64c')
         .then(res => res.json()) // parse GET output to JSON
         .then(
@@ -37,12 +45,78 @@ class App extends React.Component {
             this.setState({ movies: data}) // updata state to our movie
         })
         .catch(console.log)
+
+
+
+
+        event.preventDefault();
     }
 
-    handleSubmit = () => {
-        const {title} =  this.state;
-        // this.props.handleSendRequest(title);
-        this.setState({title: ''});
+    render() {
+        return (
+            <nav className="navbar navbar-light bg-light justify-content-betwee">
+            <a className="navbar-brand">Movie Info</a>
+            <form className="form-inline">
+                <input className="form-control mr-sm-2" type="search" placeholder="Movie title" aria-label="Movie title"
+                    onChange={this.handleChange}/>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.handleSubmit}>Search</button>
+            </form>
+            </nav>
+        );
+    }
+
+}
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            movies: [],
+            title: '',
+            search_title: 'Mad Max',
+            search_id: 76341 // Mad max
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async componentDidMount(){
+        const API_KEY = 'api_key=007d7263d6c65904a9285e1a3754a64c';
+        const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+        const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?';
+
+        console.log(this.state.search_title)
+        const url = SEARCH_URL + API_KEY + '&query=' + this.state.search_title;
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({search_id: data.results[0].id})
+        console.log(data.results[0]);
+
+        // Now grab movie details based on search
+        fetch(BASE_URL+data.results[0].id + '?' + API_KEY)
+        .then(res => res.json()) // parse GET output to JSON
+        .then(
+            (data) => {
+            this.setState({ movies: data}) // updata state to our movie
+        })
+        .catch(console.log)
+
+    }
+
+
+    handleChange(event) {
+        this.setState({search_title: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A movie was submitted: '  + this.state.search_title);
+        fetch('https://api.themoviedb.org/3/movie/152601?api_key=007d7263d6c65904a9285e1a3754a64c')
+        .then(res => res.json()) // parse GET output to JSON
+        .then( (data) => {
+            this.setState({ movies: data}) // updata state to our movie
+        })
+        .catch(console.log)
     }
 
     handleInputTitle = (event) => {
@@ -53,18 +127,22 @@ class App extends React.Component {
 
     render() {
         const {title} = this.state;
+        console.log("I am rendering");
         return (
             <div className="app">
                 <Jumbotron>
                 </Jumbotron>
 
-                <nav class="navbar navbar-light bg-light justify-content-betwee">
-                    <a class="navbar-brand">Movie Info</a>
-                    <form class="form-inline">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Movie title" aria-label="Movie title"/>
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                {/* <MovieForm></MovieForm> */}
+                <nav className="navbar navbar-light bg-light justify-content-betwee">
+                    <a className="navbar-brand">Movie Info</a>
+                    <form className="form-inline">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Movie title" aria-label="Movie title"
+                            onChange={this.handleChange}/>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.handleSubmit}>Search</button>
                     </form>
                 </nav>
+
 
                 <Movie movie={this.state.movies}/>
 
