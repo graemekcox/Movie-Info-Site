772833dbec1f9266a5712680e4391d8b5370f668
graemekcox <graeme.k.cox@gmail.com>
2020-04-10 21:31:30 -0400

    Imported bootstrap components to clean up look

--- src/index.js
+++ src/index.js
@@ -1,39 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-// class Movie extends React.Component {
-//     constructor(props){
-//         super(props);
-//     }
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
 
-//     render() {
-//         return (
-//             <div className="movie">
-//                 <div class="movie-body">
-//                     {/* <h2 className="movie-title">{this.props.movie.Title}</h2> */}
-//                     <h2 className="movie-title">Blade Runner</h2>
-//                     <h4 className="movie-plot"> Plot goes here</h4>
-//                 </div>
-//             </div>
-//         );
-
-//         }
-// }
 const Movie = ({movie}) => {
      const img_base = "https://image.tmdb.org/t/p/w500/";
 
     return (
     <div>
-        <center><h1>Movie List</h1></center>
-        <div className="movie">
+        <Container className="movie">
             <div class="movie-body">
                 <h2 className="movie-title">{JSON.stringify(movie.title)}</h2>
                 <img src={`${img_base}${movie.poster_path}`} alt="Poster" width="200"/>
                 <h5 className="movie-year">{JSON.stringify(movie.release_date)}</h5>
                 <h4 className="movie-plot">{JSON.stringify(movie.overview)}</h4>
             </div>
-        </div>
+        </Container>
     </div>
 )
 }
@@ -66,20 +51,20 @@ class App extends React.Component {
         this.setState({title});
     }
 
-
     render() {
-        // const title = "movie";
         const {title} = this.state;
         return (
             <div className="app">
-                <div className="search">
-                    <h3>Search movie!</h3>
-                    <input className="search-box" type="text" 
-                    onChange={this.handleInputTitle} value={title} />
-                    <input className="button" type="submit" onClick=
-                    {this.handleSubmit} value="Search" />
+                <Jumbotron>
+                </Jumbotron>
 
-                </div>
+                <nav class="navbar navbar-light bg-light justify-content-betwee">
+                    <a class="navbar-brand">Movie Info</a>
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Movie title" aria-label="Movie title"/>
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </nav>
 
                 <Movie movie={this.state.movies}/>
 
@@ -88,9 +73,6 @@ class App extends React.Component {
     }
 }
 
-// sendRequest = (title) => {
-//     const req = unireset("GET", "https://omdbapi.com/?t=Blade+runner");
-// }
 
 ReactDOM.render(
     <App />,
