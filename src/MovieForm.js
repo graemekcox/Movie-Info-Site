a34483aac9aecfe1f18eb61372e298fbcc09b82d
graemekcox <graeme.k.cox@gmail.com>
2020-04-11 21:46:51 -0400

    Movie search component to separate file

---
+++ src/MovieForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MovieForm extends React.Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-light bg-light justify-content-betwee">
+            <a className="navbar-brand">Movie Info</a>
+            <form className="form-inline">
+                <input className="form-control mr-sm-2" type="search" placeholder="Movie title" aria-label="Movie title"
+                    onChange={this.props.change_action}/>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.props.submit_action}>Search</button>
+            </form>
+            </nav>
+        );
+    }
+}
+export default MovieForm
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
+import MovieForm from './MovieForm.js'
 
 const Movie = ({movie}) => {
      const img_base = "https://image.tmdb.org/t/p/w500/";
@@ -21,22 +22,6 @@ const Movie = ({movie}) => {
         </Container>
     </div>
     )
-}
-
-class MovieForm extends React.Component {
-
-    render() {
-        return (
-            <nav className="navbar navbar-light bg-light justify-content-betwee">
-            <a className="navbar-brand">Movie Info</a>
-            <form className="form-inline">
-                <input className="form-control mr-sm-2" type="search" placeholder="Movie title" aria-label="Movie title"
-                    onChange={this.props.change_action}/>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.props.submit_action}>Search</button>
-            </form>
-            </nav>
-        );
-    }
 }
 
 class App extends React.Component {
